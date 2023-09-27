409dc8f0106862ddf5185531b462df4df431fda7
WandaSimeoli <wandasimeoli@gmail.com>
2023-09-27 18:25:25 +0200

    Api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,8 +4,24 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    //
+    public function index() {
+        $projects = Project::all();
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+
+    }
+    public function show(string $slug) {
+        $project = Project::where('slug', $slug)->first();
+        return response()->json([
+            'success' => true,
+            'result' => $project
+        ]);
+
+    }
 }
