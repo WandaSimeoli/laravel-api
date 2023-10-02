9951f86a1299e70eb3065841ea8e1a2d5757d656
WandaSimeoli <wandasimeoli@gmail.com>
2023-10-02 22:58:06 +0200

    Send Email

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\ContactController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProjectController;
+use App\Models\Contact;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,9 +22,9 @@ Route::name('api.')->group(function(){
         'index',
         'show'
     ]);
-
     });
 
+Route::project('/contacts', [ContactController::class, 'store'])->name('contacts.store');
 //  Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //      return $request->user();
 // });
